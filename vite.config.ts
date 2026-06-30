d23865566c355b703f793b08b7a36fec9f28ff3c
ARIF\arifa <arif18052@gmail.com>
2026-06-30 18:32:47 +0530

    Setup GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/3d-portfolio-arif/",
   plugins: [react()],
   build: {
     rollupOptions: {

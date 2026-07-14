4c3d8041c8312cc75a9e3714e7a12561034edddd
ARIF\arifa <arif18052@gmail.com>
2026-07-14 23:17:20 +0530

    General cleanup and refactoring

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <div className="about-section" id="about">
       <div className="about-me">
-        <h3 className="title">About Me</h3>
-        <p className="para">
+        <h3>About Me</h3>
+        <p>
           I am a Full Stack Developer passionate about building scalable backend systems and clean, intuitive user interfaces. With extensive experience in Java, Spring Boot, Django, React, PostgreSQL, and modern web development, I thrive on designing robust REST APIs and deploying applications using Git and Docker. Outside of product development, I enjoy sharpening my problem-solving skills by tackling complex algorithmic challenges on LeetCode.
         </p>
       </div>
